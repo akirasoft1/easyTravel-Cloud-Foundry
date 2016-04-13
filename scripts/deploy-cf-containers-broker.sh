b544a0afc26637d960f7216e29c60531d1161fe6
Martin Etmajer <m.etmajer@gmail.com>
2016-04-13 14:10:37 +0200

    Use 'dynatrace/easytravel-mongodb' Docker image to simplify deployments. Change service name to 'easytravel-mongodb'.

--- scripts/deploy-cf-containers-broker.sh
+++ scripts/deploy-cf-containers-broker.sh
@@ -26,23 +26,6 @@ deploy-cf-containers-broker-config() {
   return $?
 }
 
-deploy-mongodb-easytravel-db() {
-  local NAME="$1"
-  local DEPLOY_HOME="$2"
-
-  local ET_DB_HOME=/easyTravel-data
-  local DEPLOY_CMD="
-    mkdir -pv ${ET_DB_HOME} && \
-    cd ${ET_DB_HOME} && \
-    tar xvzf ~/deploy/data/easyTravel-mongodb-db.tar.gz && \
-    chown -vR ubuntu:ubuntu ${ET_DB_HOME}
-  "
-
-  docker-machine scp -r app/easyTravel/deploy/data ${NAME}:${DEPLOY_HOME} && \
-  docker-machine ssh ${NAME} sudo -- sh -c "'${DEPLOY_CMD}'"
-  return $?
-}
-
 run-cf-containers-broker() {
   local NAME="$1"
 
@@ -63,10 +46,6 @@ fi
 
 if ! deploy-cf-containers-broker-config "${DOCKER_MACHINE_NAME}" "${DOCKER_MACHINE_DEPLOY_HOME}"; then
   echo "Error: could not deploy cf-containers-broker configuration"; exit 1
-fi
-
-if ! deploy-mongodb-easytravel-db "${DOCKER_MACHINE_NAME}" "${DOCKER_MACHINE_DEPLOY_HOME}"; then
-  echo "Error: could not deploy MongoDB easyTravel database"; exit 1
 fi
 
 if ! run-cf-containers-broker "${DOCKER_MACHINE_NAME}"; then
