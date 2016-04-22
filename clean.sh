44ea3877cfab9c4598fbd4ee81deb9ca918cd777
Martin Etmajer <m.etmajer@gmail.com>
2016-04-22 14:38:49 +0200

    Join shell commands.

--- clean.sh
+++ clean.sh
@@ -4,6 +4,6 @@
 ./scripts/cf-target.sh && \
 cf delete easytravel-backend -f && \
 cf delete easytravel-frontend -f && \
-cf delete-service "${CF_ET_MONGODB_SERVICE}" -f &&
+cf delete-service "${CF_ET_MONGODB_SERVICE}" -f && \
 cf delete-service "${CF_DT_APPMON_SERVICE}" -f
 exit $?
